c8a26f6ec5200579e5347f7c2596bfcf6bc7d19f
Michał Drzewiecki <michal.drzewiecki@sap.com>
2023-09-07 14:39:03 +0200

    Adds sample tests without actual implementation

---
+++ pkg/reconciler/verify_reconciler_test.go
@@ -0,0 +1,17 @@
+package provisioning
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestReconciler(t *testing.T) {
+	t.Run("TBD: some sample test related to reconciler", func(t *testing.T) {
+		// given
+
+		// when
+
+		// then
+		require.True(t, true)
+	})
+}
