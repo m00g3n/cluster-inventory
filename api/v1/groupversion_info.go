0a41c1de9ad71b4db6cf3851c8240c58b5f78241
Michał Drzewiecki <michal.drzewiecki@sap.com>
2023-09-13 09:34:16 +0200

    rename to infrastructure manager

--- api/v1/groupversion_info.go
+++ api/v1/groupversion_info.go
@@ -14,9 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// Package v1 contains API Schema definitions for the clusterinventory v1 API group
+// Package v1 contains API Schema definitions for the infrastructuremanager v1 API group
 // +kubebuilder:object:generate=true
-// +groupName=clusterinventory.kyma-project.io
+// +groupName=infrastructuremanager.kyma-project.io
 package v1
 
 import (
@@ -26,7 +26,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "clusterinventory.kyma-project.io", Version: "v1"} //nolint:gochecknoglobals
+	GroupVersion = schema.GroupVersion{Group: "infrastructuremanager.kyma-project.io", Version: "v1"} //nolint:gochecknoglobals
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion} //nolint:gochecknoglobals
