fc68f9a7be365ac4dd5b47b33a086bf6f44fa25c
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2023-09-22 11:58:48 +0200

    First version of kubeconfig fetching implemented

--- cmd/main.go
+++ cmd/main.go
@@ -18,8 +18,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
+	"time"
 
+	gardener_apis "github.com/gardener/gardener/pkg/client/core/clientset/versioned/typed/core/v1beta1"
 	infrastructuremanagerv1 "github.com/kyma-project/infrastructure-manager/api/v1"
 	"github.com/kyma-project/infrastructure-manager/internal/controller"
 	"github.com/kyma-project/infrastructure-manager/internal/gardener"
@@ -30,6 +33,7 @@ import (
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 )
@@ -51,6 +55,8 @@ func main() {
 	var enableLeaderElection bool
 	var probeAddr string
 	var gardenerKubeconfigPath string
+	var gardenerProjectName string
+	var expirationInHours int64
 
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
@@ -58,7 +64,8 @@ func main() {
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
 	flag.StringVar(&gardenerKubeconfigPath, "gardener-kubeconfig-path", "/gardener/kubeconfig/kubeconfig", "Kubeconfig file for Gardener cluster")
-
+	flag.StringVar(&gardenerProjectName, "gardener-project-name", "gardener-project", "Name of the Gardener project")
+	flag.Int64Var(&expirationInHours, "expiration-in-hours", 12, "Dynamic kubeconfig expiration time in seconds")
 	opts := zap.Options{
 		Development: true,
 	}
@@ -93,9 +100,16 @@ func main() {
 		os.Exit(1)
 	}
 
-	provider := gardener.KubeconfigProvider{}
+	gardenerNamespace := fmt.Sprintf("garden-%s", gardenerProjectName)
+	expirationInSeconds := int64(time.Duration(expirationInHours).Seconds())
+	kubeconfigProvider, err := setupKubernetesKubeconfigProvider(gardenerKubeconfigPath, gardenerNamespace, expirationInSeconds)
 
-	if err = (controller.NewGardenerClusterController(mgr, provider, logger)).SetupWithManager(mgr); err != nil {
+	if err != nil {
+		setupLog.Error(err, "unable to initialize kubeconfig provider", "controller", "GardenerCluster")
+		os.Exit(1)
+	}
+
+	if err = (controller.NewGardenerClusterController(mgr, kubeconfigProvider, logger)).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "GardenerCluster")
 		os.Exit(1)
 	}
@@ -110,15 +124,34 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = gardener.NewClientFromFile(gardenerKubeconfigPath)
-	if err != nil {
-		setupLog.Error(err, "failed to load Gardener kubeconfig")
-		os.Exit(1)
-	}
-
 	setupLog.Info("starting manager")
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
 		setupLog.Error(err, "problem running manager")
 		os.Exit(1)
 	}
+}
+
+func setupKubernetesKubeconfigProvider(kubeconfigPath string, namespace string, expirationInSeconds int64) (gardener.KubeconfigProvider, error) {
+	restConfig, err := gardener.NewRestConfigFromFile(kubeconfigPath)
+	if err != nil {
+		return gardener.KubeconfigProvider{}, err
+	}
+
+	gardenerClientSet, err := gardener_apis.NewForConfig(restConfig)
+	if err != nil {
+		return gardener.KubeconfigProvider{}, err
+	}
+
+	gardenerClient, err := client.New(restConfig, client.Options{})
+	if err != nil {
+		return gardener.KubeconfigProvider{}, err
+	}
+
+	shootClient := gardenerClientSet.Shoots(namespace)
+	dynamicKubeconfigAPI := gardenerClient.SubResource("adminkubeconfig")
+
+	return gardener.NewKubeconfigProvider(shootClient,
+		dynamicKubeconfigAPI,
+		namespace,
+		expirationInSeconds), nil
 }
--- internal/gardener/client.go
+++ internal/gardener/client.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"os"
 
-	gardener_apis "github.com/gardener/gardener/pkg/client/core/clientset/versioned/typed/core/v1beta1"
+	restclient "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-func NewClientFromFile(kubeconfigFilePath string) (*gardener_apis.CoreV1beta1Client, error) {
+func NewRestConfigFromFile(kubeconfigFilePath string) (*restclient.Config, error) {
 	rawKubeconfig, err := os.ReadFile(kubeconfigFilePath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read Gardener Kubeconfig from path %s: %s", kubeconfigFilePath, err.Error())
@@ -19,10 +19,5 @@ func NewClientFromFile(kubeconfigFilePath string) (*gardener_apis.CoreV1beta1Cli
 		return nil, err
 	}
 
-	clientset, err := gardener_apis.NewForConfig(restConfig)
-	if err != nil {
-		return nil, err
-	}
-
-	return clientset, nil
+	return restConfig, err
 }
