e196760924df66cdb53f1dcb6406115c7878dd69
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2024-05-20 12:42:10 +0200

    Revert "Added AWS provider specific config"
    This reverts commit a7b6098a150318c22d8cdb44e8d79e206fdd178e.

--- internal/gardener/shoot/hyperscaler/aws/provider_specific_config.go
+++ internal/gardener/shoot/hyperscaler/aws/provider_specific_config.go
@@ -2,42 +2,12 @@ package aws
 
 import (
 	"encoding/json"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-const infrastructureConfigKind = "InfrastructureConfig"
-const controlPlaneConfigKind = "ControlPlaneConfig"
-const apiVersion = "aws.provider.extensions.gardener.cloud/v1alpha1"
-
-func GetInfrastructureConfig(workersCidr string, zones []string) ([]byte, error) {
-	return json.Marshal(NewInfrastructureConfig(workersCidr, zones))
+func GetInfrastructureConfig(_ string, _ []string) ([]byte, error) {
+	return json.Marshal("{}")
 }
 
 func GetControlPlaneConfig() ([]byte, error) {
-	return json.Marshal(NewControlPlaneConfig())
-}
-
-func NewInfrastructureConfig(workersCidr string, zones []string) InfrastructureConfig {
-	return InfrastructureConfig{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       infrastructureConfigKind,
-			APIVersion: apiVersion,
-		},
-		Networks: Networks{
-			Zones: generateAWSZones(workersCidr, zones),
-			VPC: VPC{
-				CIDR: &workersCidr,
-			},
-		},
-	}
-}
-
-func NewControlPlaneConfig() *ControlPlaneConfig {
-	return &ControlPlaneConfig{
-		TypeMeta: v1.TypeMeta{
-			Kind:       controlPlaneConfigKind,
-			APIVersion: apiVersion,
-		},
-	}
+	return json.Marshal("{}")
 }
