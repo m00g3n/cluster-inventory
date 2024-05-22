fbc48d0509d3f36ce04f3d19d20a45376838542c
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2024-05-22 16:06:28 +0200

    First implementation fo the provider specific config for Azure

--- internal/gardener/shoot/hyperscaler/azure/providerspecificconfig.go
+++ internal/gardener/shoot/hyperscaler/azure/providerspecificconfig.go
@@ -1,9 +1,49 @@
 package azure
 
-func GetInfrastructureConfig(_ string, _ []string) ([]byte, error) {
-	return nil, nil
+import (
+	"encoding/json"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const infrastructureConfigKind = "InfrastructureConfig"
+const controlPlaneConfigKind = "ControlPlaneConfig"
+const apiVersion = "azure.provider.extensions.gardener.cloud/v1alpha1"
+
+func GetInfrastructureConfig(workerCIDR string, zones []string) ([]byte, error) {
+	return json.Marshal(NewInfrastructureConfig(workerCIDR, zones))
 }
 
 func GetControlPlaneConfig() ([]byte, error) {
-	return nil, nil
+	return json.Marshal(NewControlPlaneConfig())
+}
+
+func NewControlPlaneConfig() *ControlPlaneConfig {
+	return &ControlPlaneConfig{
+		TypeMeta: v1.TypeMeta{
+			Kind:       controlPlaneConfigKind,
+			APIVersion: apiVersion,
+		},
+	}
+}
+
+func NewInfrastructureConfig(workerCIDR string, zones []string) InfrastructureConfig {
+	isZoned := len(zones) > 0
+
+	azureConfig := InfrastructureConfig{
+		TypeMeta: v1.TypeMeta{
+			Kind:       infrastructureConfigKind,
+			APIVersion: apiVersion,
+		},
+		Networks: NetworkConfig{
+			VNet: VNet{
+				CIDR: &workerCIDR,
+			},
+		},
+		Zoned: isZoned,
+	}
+
+	azureConfig.Networks.Zones = generateAzureZones(workerCIDR, zones)
+
+	return azureConfig
 }
--- internal/gardener/shoot/hyperscaler/azure/zones.go
+++ internal/gardener/shoot/hyperscaler/azure/zones.go
@@ -2,43 +2,54 @@ package azure
 
 import (
 	"math/big"
-	"math/rand"
 	"net/netip"
+	"strconv"
 )
 
-func generateRandomAzureZones(zonesCount int) []int {
-	zones := []int{1, 2, 3}
-	if zonesCount > 3 {
-		zonesCount = 3
-	}
+const defaultConnectionTimeOutMinutes = 4
+const workersBits = 3
+const cidrLength = 32
 
-	rand.Shuffle(len(zones), func(i, j int) { zones[i], zones[j] = zones[j], zones[i] })
-	return zones[:zonesCount]
-}
-
-func generateAzureZones(workerCidr string, zoneNames []int) []*Zone {
-	var zones []*Zone
+func generateAzureZones(workerCidr string, zoneNames []string) []Zone {
+	var zones []Zone
 
 	cidr, _ := netip.ParsePrefix(workerCidr)
-	workerPrefixLength := cidr.Bits() + 3
+	workerPrefixLength := cidr.Bits() + workersBits
 	workerPrefix, _ := cidr.Addr().Prefix(workerPrefixLength)
 	// delta - it is the difference between CIDRs of two zones:
 	//    zone1:   "10.250.0.0/19",
 	//    zone2:   "10.250.32.0/19",
 	delta := big.NewInt(1)
-	delta.Lsh(delta, uint(32-workerPrefixLength))
+	delta.Lsh(delta, uint(cidrLength-workerPrefixLength))
 
 	// zoneIPValue - it is an integer, which is based on IP bytes
 	zoneIPValue := new(big.Int).SetBytes(workerPrefix.Addr().AsSlice())
 
-	for _, name := range zoneNames {
+	for _, name := range convertZoneNames(zoneNames) {
 		zoneWorkerIP, _ := netip.AddrFromSlice(zoneIPValue.Bytes())
 		zoneWorkerCidr := netip.PrefixFrom(zoneWorkerIP, workerPrefixLength)
 		zoneIPValue.Add(zoneIPValue, delta)
-		zones = append(zones, &Zone{
+		zones = append(zones, Zone{
 			Name: name,
 			CIDR: zoneWorkerCidr.String(),
+			NatGateway: &NatGateway{
+				Enabled:                      true,
+				IdleConnectionTimeoutMinutes: defaultConnectionTimeOutMinutes,
+			},
 		})
 	}
+	return zones
+}
+
+func convertZoneNames(zoneNames []string) []int {
+	var zones []int
+	for _, inputZone := range zoneNames {
+		zone, err := strconv.Atoi(inputZone)
+		if err != nil || zone < 1 || zone > 3 {
+			continue
+		}
+		zones = append(zones, zone)
+	}
+
 	return zones
 }
