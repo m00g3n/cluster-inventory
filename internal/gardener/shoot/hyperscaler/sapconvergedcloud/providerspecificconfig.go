154118da3dadb4e52de5a777c10b3e9197244c8d
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2024-06-03 07:19:49 +0200

    Started working on openstack support

--- internal/gardener/shoot/hyperscaler/sapconvergedcloud/providerspecificconfig.go
+++ internal/gardener/shoot/hyperscaler/sapconvergedcloud/providerspecificconfig.go
@@ -1,9 +1,38 @@
 package sapconvergedcloud
 
+import (
+	"github.com/gardener/gardener-extension-provider-openstack/pkg/apis/openstack/v1alpha1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	infrastructureConfigKind = "InfrastructureConfig"
+	controlPlaneConfigKind   = "ControlPlaneConfig"
+	apiVersion               = "openstack.provider.extensions.gardener.cloud/v1alpha1"
+)
+
 func GetInfrastructureConfig(_ string, _ []string) ([]byte, error) {
 	return nil, nil
 }
 
 func GetControlPlaneConfig(_ []string) ([]byte, error) {
 	return nil, nil
+}
+
+func NewInfrastructureConfig(workerCIDR string) v1alpha1.InfrastructureConfig {
+	return v1alpha1.InfrastructureConfig{
+		TypeMeta: v1.TypeMeta{
+			Kind:       infrastructureConfigKind,
+			APIVersion: apiVersion,
+		},
+	}
+}
+
+func NewControlPlaneConfig(zones []string) *v1alpha1.ControlPlaneConfig {
+	return &v1alpha1.ControlPlaneConfig{
+		TypeMeta: v1.TypeMeta{
+			Kind:       controlPlaneConfigKind,
+			APIVersion: apiVersion,
+		},
+	}
 }
