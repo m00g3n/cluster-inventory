a0a0b830382f14806b05aeff0054c82165456897
Rafal Foks <r.fok74@gmail.com>
2024-08-29 11:20:43 +0200

    Adjust controller tests

--- internal/controller/runtime/runtime_controller_test.go
+++ internal/controller/runtime/runtime_controller_test.go
@@ -106,7 +106,7 @@ var _ = Describe("Runtime Controller", func() {
 					return false
 				}
 
-				if !runtime.IsConditionSet(imv1.ConditionTypeRuntimeConfigured, imv1.ConditionReasonConfigurationCompleted) {
+				if !runtime.IsConditionSet(imv1.ConditionTypeAuditLogConfigured, imv1.ConditionReasonConfigurationCompleted) {
 					return false
 				}
 
