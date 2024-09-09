eea82e36c0a16ec1d85e500a12f0124bcc2b3b9c
Rafal Foks <r.fok74@gmail.com>
2024-09-09 14:21:56 +0200

    Fix linter

--- internal/controller/runtime/fsm/runtime_fsm_configure_auditlog.go
+++ internal/controller/runtime/fsm/runtime_fsm_configure_auditlog.go
@@ -2,6 +2,7 @@ package fsm
 
 import (
 	"context"
+
 	imv1 "github.com/kyma-project/infrastructure-manager/api/v1"
 	"github.com/kyma-project/infrastructure-manager/internal/auditlogging"
 	"github.com/pkg/errors"
