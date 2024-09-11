815595afe7788ec5d4873d3a877fe8a539c313be
Arkadiusz Galwas <arkadiusz.galwas@sap.com>
2024-09-11 11:46:58 +0200

    Fixes to migrator that will remove unimportant differences

--- hack/runtime-migrator/main.go
+++ hack/runtime-migrator/main.go
@@ -206,7 +206,7 @@ func createRuntime(ctx context.Context, shoot v1beta1.Shoot, cfg migrator.Config
 				},
 				Provider: v1.Provider{
 					Type:    shoot.Spec.Provider.Type,
-					Workers: getWorkersConfig(shoot.Spec.Provider.Workers),
+					Workers: adjustWorkersConfig(shoot.Spec.Provider.Workers),
 				},
 				Networking: v1.Networking{
 					Type:     shoot.Spec.Networking.Type,
@@ -245,7 +245,7 @@ func createRuntime(ctx context.Context, shoot v1beta1.Shoot, cfg migrator.Config
 
 // The goal of this function is to make the migrator output equal to the shoot created by the converter
 // As a result we can automatically verify the correctness of the migrator output
-func getWorkersConfig(workers []v1beta1.Worker) []v1beta1.Worker {
+func adjustWorkersConfig(workers []v1beta1.Worker) []v1beta1.Worker {
 	// We need to set the following fields to nil, as they are not set by the KIM converter
 	for i := 0; i < len(workers); i++ {
 		workers[i].Machine.Architecture = nil
